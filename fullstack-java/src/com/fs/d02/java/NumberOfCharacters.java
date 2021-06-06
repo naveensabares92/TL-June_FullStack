f209492ecc7de0100da39eb3dad4616371d783eb
Naveen B Sabares <naveen.sabares@camelotls.com>
2021-06-06 15:31:07 +0530

    add multiple inputs for pgm-find characters in array

--- fullstack-java/src/com/fs/d02/java/NumberOfCharacters.java
+++ fullstack-java/src/com/fs/d02/java/NumberOfCharacters.java
@@ -4,16 +4,27 @@ public class NumberOfCharacters {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] arr = {"aa", "bb", "c", "dd"};
-		int countToFind = 2;
+		
+		//Input Arrays
+		String[] arr1 = {"aa", "bb", "c", "dd"};
+		String[] arr2 = {"aaa", "bb", "ccc", "ddd"};
+		String[] arr3 = {"aaaa", "b", "c", "ddd"};
+		
+		getNumberOfCharacters(arr1, 2);
+		getNumberOfCharacters(arr2, 1);
+		getNumberOfCharacters(arr3, 4);
+	}
+	
+	public static void getNumberOfCharacters(String[] arrayInput, int countToFind) {
 		int counter = 0;
-		for (String text : arr) {
-			if(text.length() == countToFind) {
+		
+		for (String textUnderValidation : arrayInput) {
+			if(textUnderValidation.length() == countToFind) {
 				counter++;
 			}
 		}
 		
-		System.out.println("Number of String with " + countToFind + " are " + counter );	
+		System.out.println("Number of Strings with " + countToFind + " characters are " + counter );	
 	}
 
 }
