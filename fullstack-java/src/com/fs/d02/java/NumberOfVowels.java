4ff469ebe588fc0055c7589f5cb4423d3bb49eaa
Naveen B Sabares <naveen.sabares@camelotls.com>
2021-06-07 20:22:21 +0530

    vowels pgm corrected to find only 1 occurrance

--- fullstack-java/src/com/fs/d02/java/NumberOfVowels.java
+++ fullstack-java/src/com/fs/d02/java/NumberOfVowels.java
@@ -3,37 +3,20 @@ package com.fs.d02.java;
 public class NumberOfVowels {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] caseSensitiveVowels = getCaseSensitiveVowels();
 		
 		String strtoVerify = "NewyorkE iOU";
+		String[] vowels = {"a", "e", "i", "o", "u"};
 		int countOfVowels = 0;
-		
-		for (char alphabetUnderTest : strtoVerify.toCharArray()) {
-			for(String vowelUnderTest: caseSensitiveVowels) {
+
+		for(String vowelUnderTest: vowels) {
+			for (char alphabetUnderTest : strtoVerify.replace(" ","").toLowerCase().toCharArray()) {
 				if(alphabetUnderTest == vowelUnderTest.charAt(0)) {
 					countOfVowels++;
 					break;
 				}
-			}
+			}	
 		}
 		
-		
-		System.out.println("Total Vowels - " + countOfVowels);
-		
-		
+		System.out.println("Total # of vowels - " + countOfVowels);		
 	}
-	
-	public static String[] getCaseSensitiveVowels() {
-		String[] vowels = {"a", "e", "i", "o", "u"};
-		String[] newVowels = new String[10];
-		
-		for(int i = 0; i < vowels.length ; i++) {
-			newVowels[i+i] = vowels[i];
-			newVowels[i+i+1] = vowels[i].toUpperCase();
-		}
-		
-		return newVowels;
-	}
-
 }
---
+++ fullstack-java/src/com/fs/d02/java/SquareRootCalculation.java
@@ -0,0 +1,22 @@
+package com.fs.d02.java;
+
+public class SquareRootCalculation {
+
+	public static void main(String[] args) {
+		double x1=5; 
+		double x2=6; 
+		double y1=8; 
+		double y2=3;
+
+		double resultingNumber;
+		int squareRoot;
+		
+		resultingNumber = ((x1+x2) * (x1+x2)) + ((y1+y2) * (y1+y2));
+		
+		System.out.println(resultingNumber);
+		
+		//TODO find square root 
+		
+	}
+
+}
